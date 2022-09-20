269767b08bde992c2886b7dfe4facc68ee5978a6
gabisegger <gabi.segger@gmail.com>
2022-09-20 08:17:39 -0300

    chore: video do hotel com autoplay

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from './Home.module.css';
 import logo from '../../assets/zebra.png'
+import video from '../../assets/hotel-transilvania.mp4'
 
 export default function Home() {
+    useEffect(() => {
+        window.addEventListener("load", function ()
+            {
+                let video = window.document.querySelector("#video");
+                video.play();
+                video.loop = true;
+            });
+    }, [])
+    
     return (
         <div className={style.category}>
             <h1 className={style.title}>Hotel <img className={style.logo} src={logo} alt="Logo"/>Transilvânia</h1>
             <h2 className={style.subtitle}>Colecione bons momentos com a gente</h2>
             <p className={style.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-            <div>
+            <div className={style.video}>
+                <video autoplay="autoplay" loop="loop" controls src={video} type="video/mp4 controls" height="315" width="560" ></video>
             </div>
         </div> 
     );
